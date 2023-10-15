d01707256edffba2e975582becd471b5f66a98cf
david87chdz <david87chdz@gmail.com>
2023-10-15 23:06:38 +0200

    6546541

--- Ejer1.js
+++ Ejer1.js
@@ -19,17 +19,51 @@ let palabra= (palabra) =>{
 
 palabra("al");
 
-function isPalindromo(element){
-    let new_word = [...element].reverse().join('');
-    if(element === new_word){
-        console.log(`${element} is palindromo`);
-    }else{
-        console.log(`${element} isn't palindromo`);
-    } 
-}
-isPalindromo('bob');
 
 //EJERCICIO pasar una oracion y una palabra, devolver cuantas veces aparece esa palabra en la oracion
 let vasdf=(oracion, palabra) =>{
-    
+    let dividida= oracion.split(" ");
+    let contador=0;
+    for(let pala of dividida){
+        console.log(pala);
+        if(pala===palabra){
+            contador++;
+        }
+    }
+    console.log("la palabra "+palabra+ " aparece "+contador+" veces");
+};
+
+vasdf('hola? hola hola hola susana', 'hola'); //mejorar las que no coge como la primeroa y la ultima
+
+//EJERCICIO recibir una cadena y devolverla al reves pero sin usar reverse, ni split 
+
+let cadenaInv=(cadena)=>{
+    let inversa= "";
+    for( let i=cadena.length; i>=0; i--){
+        inversa+=cadena.charAt(i);
+    }
+    console.log(inversa);
+};
+
+cadenaInv("hola pepe");
+
+// EJERCICIO cual es el X% de X numero ?
+let porcentaje= (porcentaje, x)=>{
+    let porCiento= porcentaje /100 *x;
+    return "El "+porcentaje+ " por ciento de " +x+" es igual a "+porCiento;
 }
+
+let a = porcentaje(20, 100);
+console.log(a);
+
+//EJERCICIO pasar un numero a la funcion y hacer un cuadrado de * que sea num de ancho y num de alto con el medio vacio
+
+let nume= 4;
+for(let i=0; i<nume;i++){
+    for(let j=0; j<nume;j++){
+        if(i==0 || j==0){
+            console.log('*')
+        }
+        
+    }
+};
